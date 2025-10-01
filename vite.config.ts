2af90f335d16c9c02d6bd9e11ad31efcbcfb724a
Libeyondea <57558120+libeyondea@users.noreply.github.com>
2025-10-02 00:58:12 +0700

    up lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,48 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), dts({ include: ['src'] })],
 	resolve: {
 		alias: {
 			'~': path.resolve(__dirname, 'src')
+		}
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.js'),
+			name: 'BaseCms',
+			fileName: (format) => `base-cms.${format}.js`,
+			formats: ['es', 'umd']
+		},
+		rollupOptions: {
+			external: [
+				'react',
+				'react-dom',
+				'react/jsx-runtime',
+				'@mui/material',
+				'@mui/icons-material',
+				'@emotion/react',
+				'@emotion/styled',
+				'@reduxjs/toolkit',
+				'react-redux',
+				'react-router-dom'
+			],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'@mui/material': 'MaterialUI',
+					'@emotion/react': 'EmotionReact',
+					'@emotion/styled': 'EmotionStyled',
+					'@reduxjs/toolkit': 'ReduxToolkit',
+					'react-redux': 'ReactRedux',
+					'react-router-dom': 'ReactRouterDOM'
+				}
+			}
 		}
 	}
 });
